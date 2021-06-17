595e2c3d48c29c71e3dbf0bf1d48388c2d269cfe
Tu Anh Dinh <tad@mediaan.com>
2021-06-17 23:30:40 +0700

    Update shell script to also evaluate SVCCA for language similarity

--- modality_similarity_svcca.sh
+++ modality_similarity_svcca.sh
@@ -16,7 +16,7 @@ if [ -d ${ACTIVATIONS_PATH} ]; then
 fi
 mkdir ${ACTIVATIONS_PATH}
 # Save activations
-mkdir ${ACTIVATIONS_PATH}/audio_activation
+mkdir ${ACTIVATIONS_PATH}/en_audio_activation
 echo "Save audio activations"
 python translate.py -model ${MODEL_DIR}/$CHOSEN_MODEL_NAME \
     -src $DATA_DIR/${SRC_LANG}_audio_test.scp \
@@ -30,25 +30,41 @@ python translate.py -model ${MODEL_DIR}/$CHOSEN_MODEL_NAME \
     -batch_size 5 \
     -max_sent_length 1024 \
     -gpu 0 \
-    -save_activation $ACTIVATIONS_PATH/audio_activation
-python post_process_activations.py -activations_dir $ACTIVATIONS_PATH/audio_activation \
-    -save_activation $ACTIVATIONS_PATH/audio_activation.pt
-rm -r $ACTIVATIONS_PATH/audio_activation
-echo "Save text activations"
-mkdir ${ACTIVATIONS_PATH}/text_activation
+    -save_activation $ACTIVATIONS_PATH/en_audio_activation
+python post_process_activations.py -activations_dir $ACTIVATIONS_PATH/en_audio_activation \
+    -save_activation $ACTIVATIONS_PATH/en_audio_activation.pt
+rm -r $ACTIVATIONS_PATH/en_audio_activation
+echo "Save en text activations"
+mkdir ${ACTIVATIONS_PATH}/en_text_activation
 python translate.py -model ${MODEL_DIR}/$CHOSEN_MODEL_NAME \
     -src $DATA_DIR/${SRC_LANG}_text_test.txt \
     -src_lang $SRC_LANG \
     -tgt_lang $TGT_LANG \
     -encoder_type text \
     -tgt $DATA_DIR/${TGT_LANG}_text_test.txt  \
-    -output ${ACTIVATIONS_PATH}/encoded_translations_mt.txt \
+    -output ${ACTIVATIONS_PATH}/encoded_translations_ende_mt.txt \
     -batch_size 5 \
     -max_sent_length 1024 \
     -gpu 0 \
-    -save_activation $ACTIVATIONS_PATH/text_activation
-python post_process_activations.py -activations_dir $ACTIVATIONS_PATH/text_activation \
-    -save_activation $ACTIVATIONS_PATH/text_activation.pt
-rm -r $ACTIVATIONS_PATH/text_activation
+    -save_activation $ACTIVATIONS_PATH/en_text_activation
+python post_process_activations.py -activations_dir $ACTIVATIONS_PATH/en_text_activation \
+    -save_activation $ACTIVATIONS_PATH/en_text_activation.pt
+rm -r $ACTIVATIONS_PATH/en_text_activation
+echo "Save de text activations"
+mkdir ${ACTIVATIONS_PATH}/de_text_activation
+python translate.py -model ${MODEL_DIR}/$CHOSEN_MODEL_NAME \
+    -src $DATA_DIR/${TGT_LANG}_text_test.txt \
+    -src_lang $TGT_LANG \
+    -tgt_lang $TGT_LANG \
+    -encoder_type text \
+    -tgt $DATA_DIR/${TGT_LANG}_text_test.txt  \
+    -output ${ACTIVATIONS_PATH}/encoded_translations_dede_mt.txt \
+    -batch_size 5 \
+    -max_sent_length 1024 \
+    -gpu 0 \
+    -save_activation $ACTIVATIONS_PATH/de_text_activation
+python post_process_activations.py -activations_dir $ACTIVATIONS_PATH/de_text_activation \
+    -save_activation $ACTIVATIONS_PATH/de_text_activation.pt
+rm -r $ACTIVATIONS_PATH/de_text_activation
 # Inspect SVCCA
 python svcca/inspect_st.py $ACTIVATIONS_PATH 50 | tee ${ACTIVATIONS_PATH}/svcca_output.txt
